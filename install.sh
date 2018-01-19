785791eff6e9d057c414600e183b20d8baeef7c6
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2018-01-19 23:48:46 +0100

    Complete install

--- install.sh
+++ install.sh
@@ -43,6 +43,63 @@ drush role-create lid
 #hier komt de uitbreiding permissions - hieronder voorbeeld
 #drush role-add-perm 'lid' 'access user contact forms'
 
+
+drush role-add-perm 'lid' 'access own webform results'
+drush role-add-perm 'lid' 'access own webform submissions'
+drush role-add-perm 'lid' 'access user contact forms'
+drush role-add-perm 'lid' 'access user profiles'
+drush role-add-perm 'lid' 'change own username'
+drush role-add-perm 'lid' 'edit own webform submissions'
+
+drush role-add-perm 'medewerker' 'access all webform results'
+drush role-add-perm 'medewerker' 'access CiviContribute'
+drush role-add-perm 'medewerker' 'access CiviCRM'
+drush role-add-perm 'medewerker' 'access CiviEvent'
+drush role-add-perm 'medewerker' 'access CiviMail'
+drush role-add-perm 'medewerker' 'access CiviMember'
+drush role-add-perm 'medewerker' 'access CiviReport'
+drush role-add-perm 'medewerker' 'access deleted contacts'
+drush role-add-perm 'medewerker' 'access own webform results'
+drush role-add-perm 'medewerker' 'access own webform submissions'
+drush role-add-perm 'medewerker' 'access Report Criteria'
+drush role-add-perm 'medewerker' 'access user profiles'
+drush role-add-perm 'medewerker' 'add contacts'
+drush role-add-perm 'medewerker' 'administer users'
+drush role-add-perm 'medewerker' 'change own username'
+drush role-add-perm 'medewerker' 'create page content'
+drush role-add-perm 'medewerker' 'create vergaderstuk_ken_ content'
+drush role-add-perm 'medewerker' 'delete activities'
+drush role-add-perm 'medewerker' 'delete any page content'
+drush role-add-perm 'medewerker' 'delete any vergaderstuk_ken_ content'
+drush role-add-perm 'medewerker' 'delete contacts'
+drush role-add-perm 'medewerker' 'delete in CiviEvent'
+drush role-add-perm 'medewerker' 'delete in CiviMail'
+drush role-add-perm 'medewerker' 'delete own page content'
+drush role-add-perm 'medewerker' 'delete own vergaderstuk_ken_ content'
+drush role-add-perm 'medewerker' 'edit all contacts'
+drush role-add-perm 'medewerker' 'edit all events'
+drush role-add-perm 'medewerker' 'edit all webform submissions'
+drush role-add-perm 'medewerker' 'edit any page content'
+drush role-add-perm 'medewerker' 'edit any vergaderstuk_ken_ content'
+drush role-add-perm 'medewerker' 'edit contributions'
+drush role-add-perm 'medewerker' 'edit event participants'
+drush role-add-perm 'medewerker' 'edit groups'
+drush role-add-perm 'medewerker' 'edit memberships'
+drush role-add-perm 'medewerker' 'edit my contact'
+drush role-add-perm 'medewerker' 'edit own page content'
+drush role-add-perm 'medewerker' 'edit own vergaderstuk_ken_ content'
+drush role-add-perm 'medewerker' 'edit own webform submissions'
+drush role-add-perm 'medewerker' 'manage tags'
+drush role-add-perm 'medewerker' 'masquerade as user'
+drush role-add-perm 'medewerker' 'merge duplicate contacts'
+drush role-add-perm 'medewerker' 'search content'
+drush role-add-perm 'medewerker' 'use text format full_html'
+drush role-add-perm 'medewerker' 'view all activities'
+drush role-add-perm 'medewerker' 'view all contacts'
+drush role-add-perm 'medewerker' 'view my contact'
+drush role-add-perm 'medewerker' 'view public CiviMail content'
+
+
 echo '\n\n'
 echo 'III)  download and install CiviCRM extensions\n\n'
 
@@ -50,7 +107,7 @@ echo 'III)  download and install CiviCRM extensions\n\n'
 # Let op: het download pad wordt hier direct genoteerd. Komt er een nieuwere versie van een extensie
 # dan moet dit aangepast worden
 #
-drush cvapi Extension.download key="be.domusmedica.kringsetup" url="https://github.com/CiviCooP/be.domusmedica.kringsetup/archive/v0.2.zip" install="1"
+drush cvapi Extension.download key="be.domusmedica.kringsetup" url="https://github.com/CiviCooP/be.domusmedica.kringsetup/archive/master.zip" install="1"
 drush cvapi Extension.download key="nz.co.fuzion.csvimport" url="https://github.com/eileenmcnaughton/nz.co.fuzion.csvimport/archive/1.2.zip" install="1"
 drush cvapi Extension.download key="nz.co.fuzion.omnipaymultiprocessor" url="https://github.com/eileenmcnaughton/nz.co.fuzion.omnipaymultiprocessor/archive/2.4.zip" install="1"
 drush cvapi Extension.download key="org.civicoop.emailapi" url="https://github.com/CiviCooP/org.civicoop.emailapi/archive/V1.12.zip" install="1"
@@ -58,7 +115,7 @@ drush cvapi Extension.download key="org.civicrm.angularprofiles" install="1"
 drush cvapi Extension.download key="org.civicrm.flexmailer" url="https://github.com/civicrm/org.civicrm.flexmailer/archive/v0.2-alpha5.zip" install="1"
 drush cvapi Extension.download key="org.civicrm.shoreditch" url="https://github.com/civicrm/org.civicrm.shoreditch/archive/v0.1-alpha10-nonsafecolour.zip" install="1"
 drush cvapi Extension.download key="uk.co.vedaconsulting.mosaico" url="https://download.civicrm.org/extension/uk.co.vedaconsulting.mosaico/latest/uk.co.vedaconsulting.mosaico-latest.zip" install="1"
-drush cvapi Extension.download key="be.domusmedica.sync" url="https://github.com/CiviCooP/be.domusmedica.sync/archive/v0.1.zip" install="1"
+drush cvapi Extension.download key="be.domusmedica.sync" url="https://github.com/CiviCooP/be.domusmedica.sync/archive/master.zip" install="1"
 
 echo '\n\n'
 echo 'IV)   enable and disable CiviCRM Compents\n\n'
