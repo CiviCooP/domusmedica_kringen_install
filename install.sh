35c5c93e62c8413a537b480f46e5cc8b3f8ad78b
Klaas Eikelboom <klaas.eikelboom@civicoop.org>
2018-01-29 15:43:08 +0100

    Add privileges and content types with features

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+INSTALL_DIR=`dirname $0`
 echo 'Installing a new Domus Medica Instance'
 echo '*)    download and enable drupal modules \n\n'
 
@@ -35,74 +36,16 @@ drush en webform_layout --yes
 drush en webform_matrix_component --yes
 
 drush en civi_bartik -y
+drush en features -y
 # onderstaande maakt civi_bartik gelijk het admin thema
 # voor CiviCRM
 drush vset civicrmtheme_theme_admin civi_bartik
 
 echo '\n\n'
 echo '*)   create roles and add permisions\n\n'
+echo '     using a drupal feature'
 
-drush role-create medewerker
-drush role-create lid
-
-#hier komt de uitbreiding permissions - hieronder voorbeeld
-#drush role-add-perm 'lid' 'access user contact forms'
-
-
-drush role-add-perm 'lid' 'access own webform results'
-drush role-add-perm 'lid' 'access own webform submissions'
-drush role-add-perm 'lid' 'access user contact forms'
-drush role-add-perm 'lid' 'access user profiles'
-drush role-add-perm 'lid' 'change own username'
-drush role-add-perm 'lid' 'edit own webform submissions'
-
-drush role-add-perm 'medewerker' 'access all webform results'
-drush role-add-perm 'medewerker' 'access CiviContribute'
-drush role-add-perm 'medewerker' 'access CiviCRM'
-drush role-add-perm 'medewerker' 'access CiviEvent'
-drush role-add-perm 'medewerker' 'access CiviMail'
-drush role-add-perm 'medewerker' 'access CiviMember'
-drush role-add-perm 'medewerker' 'access CiviReport'
-drush role-add-perm 'medewerker' 'access deleted contacts'
-drush role-add-perm 'medewerker' 'access own webform results'
-drush role-add-perm 'medewerker' 'access own webform submissions'
-drush role-add-perm 'medewerker' 'access Report Criteria'
-drush role-add-perm 'medewerker' 'access user profiles'
-drush role-add-perm 'medewerker' 'add contacts'
-drush role-add-perm 'medewerker' 'administer users'
-drush role-add-perm 'medewerker' 'change own username'
-drush role-add-perm 'medewerker' 'create page content'
-drush role-add-perm 'medewerker' 'create vergaderstuk_ken_ content'
-drush role-add-perm 'medewerker' 'delete activities'
-drush role-add-perm 'medewerker' 'delete any page content'
-drush role-add-perm 'medewerker' 'delete any vergaderstuk_ken_ content'
-drush role-add-perm 'medewerker' 'delete contacts'
-drush role-add-perm 'medewerker' 'delete in CiviEvent'
-drush role-add-perm 'medewerker' 'delete in CiviMail'
-drush role-add-perm 'medewerker' 'delete own page content'
-drush role-add-perm 'medewerker' 'delete own vergaderstuk_ken_ content'
-drush role-add-perm 'medewerker' 'edit all contacts'
-drush role-add-perm 'medewerker' 'edit all events'
-drush role-add-perm 'medewerker' 'edit all webform submissions'
-drush role-add-perm 'medewerker' 'edit any page content'
-drush role-add-perm 'medewerker' 'edit any vergaderstuk_ken_ content'
-drush role-add-perm 'medewerker' 'edit contributions'
-drush role-add-perm 'medewerker' 'edit event participants'
-drush role-add-perm 'medewerker' 'edit groups'
-drush role-add-perm 'medewerker' 'edit memberships'
-drush role-add-perm 'medewerker' 'edit my contact'
-drush role-add-perm 'medewerker' 'edit own page content'
-drush role-add-perm 'medewerker' 'edit own vergaderstuk_ken_ content'
-drush role-add-perm 'medewerker' 'edit own webform submissions'
-drush role-add-perm 'medewerker' 'manage tags'
-drush role-add-perm 'medewerker' 'masquerade as user'
-drush role-add-perm 'medewerker' 'merge duplicate contacts'
-drush role-add-perm 'medewerker' 'search content'
-drush role-add-perm 'medewerker' 'use text format full_html'
-drush role-add-perm 'medewerker' 'view all activities'
-drush role-add-perm 'medewerker' 'view all contacts'
-drush role-add-perm 'medewerker' 'view my contact'
-drush role-add-perm 'medewerker' 'view public CiviMail content'
+cp -r $INSTALL_DIR/domusmedica_kringsetup sites/all/modules
 
 
 echo '\n\n'
